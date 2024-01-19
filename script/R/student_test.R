9befe68d245ccbd8116f5015181c2a4e0d0fb3fd
usama-ak <usama.akhtar@outlook.fr>
2024-01-19 16:45:51 +0100

    Create student_test.R

---
+++ script/R/student_test.R
@@ -0,0 +1,25 @@
+library(data.table)
+library(mclust)
+
+# Read data
+dS_values <- fread("C:/Users/usama/Desktop/comparativegenomics/dS_values_distances.txt")
+dS_values_filtered <- dS_values[dS_values$V3 < 3, ]
+ks_values <- as.numeric(dS_values_filtered$V3)
+
+# Fit a Gaussian Mixture Model (GMM) with two components
+gmm_model <- Mclust(ks_values, G = 2)
+
+# Perform a t-test
+t_test_result <- t.test(ks_values ~ gmm_model$classification)
+p_value <- t_test_result$p.value
+alpha <- 0.05
+
+# Print the p-value
+print(p_value)
+
+# Check for significance
+if (p_value < alpha) {
+  cat("The difference in means is statistically significant.\n")
+} else {
+  cat("The difference in means is not statistically significant.\n")
+}
