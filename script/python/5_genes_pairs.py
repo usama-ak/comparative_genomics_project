1045ca9b93477e0992e38fb64b4ace7d720961b8
usama-ak <usama.akhtar@outlook.fr>
2024-01-19 15:28:01 +0100

    Update 5_genes_pairs.py

--- script/python/5_genes_pairs.py
+++ script/python/5_genes_pairs.py
@@ -3,11 +3,10 @@
 def generate_gene_pairs(input_file, output_file):
     gene_families = []
     
-    # Read the input file and split the lines
     with open(input_file, 'r') as file:
         lines = file.readlines()
 
-    # Process each line to extract gene information
+    # Process each line as one family
     for line in lines:
         genes = line.strip().split('\t')
         gene_families.append(genes)
@@ -24,11 +23,10 @@ def generate_gene_pairs(input_file, output_file):
         for pair in gene_pairs:
             output_file.write('\t'.join(pair) + '\n')
 
-# Specify the input and output file paths
+
 input_file_path = 'output_families_30_50.txt'
 output_file_path = 'gene_pairs_output_30_50.txt'
 
-# Generate gene pairs and write to the output file
 generate_gene_pairs(input_file_path, output_file_path)
 
 
